02672d18177d022e5e6f4197ea9e5556990da32d
jkovacevic <jk.simunic@gmail.com>
2017-01-26 12:42:47 +0100

    Created util.h file

---
+++ src/util.h
@@ -0,0 +1,15 @@
+long* read_file(char* file_name);
+
+int write_file(long* file_content, long length, char* file_name);
+
+long code_char(char t);
+
+char decode_char(long code);
+
+void free_ch_suite(struct ch_suite** cs);
+
+void free_bucket(struct bucket* b);
+
+void free_bucket_suite(struct bucket_suite** bs);
+
+void free_sstar_substring_suite(struct sstar_substring_suite** ss);
