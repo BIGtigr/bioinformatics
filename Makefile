a7869228e04fc19c2f364de1ebc8801d198dca15
Toni Budrović <toni.budrovic@gmail.com>
2017-01-26 01:31:52 +0100

    Added Makefile and .gitignore

--- Makefile
+++ Makefile
@@ -1,10 +1,20 @@
 CC=gcc
-CFLAGS=-I.
-DEPS = util.h
-OBJ = bwt.o sa.o util.o
+CFLAGS=-Isrc -Itest -Wall -Werror -O2
 
-%.o: %.c $(DEPS)
+SRC_OBJ = obj/bwt.o obj/sa.o obj/util.o
+TEST_OBJ = obj/sa_test.o obj/test_suite.o obj/util_test.o obj/bwt_test.o $(SRC_OBJ)
+
+obj/%.o: src/%.c
 	$(CC) -c -o $@ $< $(CFLAGS)
 
-src: $(OBJ)
-	gcc -o $@ $^ $(CFLAGS)
+obj/%.o: test/%.c
+	$(CC) -c -o $@ $< $(CFLAGS)
+
+src: $(SRC_OBJ) obj/main.o
+	gcc -o bwt $^ $(CFLAGS)
+
+test: $(TEST_OBJ)
+	gcc -ggdb -o tests $^ $(CFLAGS)
+	./tests
+
+default: src
--- src/main.c
+++ src/main.c
@@ -1,34 +1,7 @@
-#include <string.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include "src.h"
 
-char *text = read_file();
 
-induce(char *R) { // is it really char* here? ch_suite more likely
-    right pass;
-    left pass;
-    buckets_place_sstar;
-    induce_l_suffixes;
-    induce_s_suffixes;
-}
-
-B bwt_is(text) {
-    // list of S* substrings
-    R[] = find_sstar_substrings(left_pass(right_pass(text)));
-
-    names of *S substrings = induce(R) // for each element of list, or whole list at once?
-
-    if (unique(shortened_text)) { // alphabet of shortened text does not contain repeating characters
-	B1 = directly_compute_from_shortened_text();
-    } else {
-	B1 = bwt_is(shortened_text); // text, text_length, alphabet (number of different characters);
-    }
-
-    Decode S* strings B1 into R; // using B1, determine correct order of S* substrings (comment in paper, page 3)
-    // result in R is list of S* substrings?
-	
-    B = induce(R);
-
-    return B;
+int main(int argc, char **argv) {
+    printf("Hello. You should really write some code here.\n");
+    return 0;
 }
